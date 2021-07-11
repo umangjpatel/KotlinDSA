5dc4163b32a09147d5af99a95b568744a0bb58db
umangjpatel <umangpatel1947@gmail.com>
2021-07-11 14:49:44 -0400

    SinglyLinkedList implemented with deletion operations.

--- src/main/kotlin/linked_lists/SinglyLinkedList.kt
+++ src/main/kotlin/linked_lists/SinglyLinkedList.kt
@@ -77,4 +77,63 @@ class SinglyLinkedList<T> {
         size++
         return this
     }
+
+    /**
+     * Removes a node from the head of the linked list.
+     * Also returns the value from that node.
+     * Time complexity: O(1)
+     */
+    fun pop(): T? {
+        if (!isEmpty())
+            size--
+        val result = head?.value
+        head = head?.next
+        if (isEmpty())
+            tail = null
+        return result
+    }
+
+    /**
+     * Removes a node from the tail of the linked list.
+     * Also returns the value from that node.
+     * Time complexity: O(1)
+     */
+    fun removeLast(): T? {
+        val head = head ?: return null
+        if (head.next == null)
+            return pop()
+        size--
+        var prev = head
+        var current = head
+        var next = current.next
+
+        while (next != null) {
+            prev = current
+            current = next
+            next = current.next
+        }
+        prev.next = null
+        tail = prev
+        return current.value
+    }
+
+    /**
+     * Removes a node at a particular index.
+     * Also returns the value from that node.
+     * Time complexity: O(i) where i is the index of the requested node.
+     */
+    fun removeAt(index: Int): T? {
+        if (index < 0 || index >= size)
+            return null
+        else if (index == 0)
+            return pop()
+        val beforeNode = nodeAt(index = index - 1)
+        if (tail == beforeNode?.next)
+            return removeLast()
+        val result = beforeNode?.next?.value
+        beforeNode?.next = beforeNode?.next?.next
+        size--
+        return result
+    }
+
 }
