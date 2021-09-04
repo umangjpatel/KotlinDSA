fa6eaccbcc1d627088c336f601307251b9f48b04
umangjpatel <umangpatel1947@gmail.com>
2021-09-04 16:23:21 -0400

    DoublyLinkedList operations implemented

--- src/main/kotlin/basic/linkedlists/DoublyLinkedList.kt
+++ src/main/kotlin/basic/linkedlists/DoublyLinkedList.kt
@@ -1,14 +1,160 @@
 package basic.linkedlists
 
-class DoublyLinkedList<T> {
-    private var head: DoublyNode<T>? = null
-    private var tail: DoublyNode<T>? = null
+import basic.linkedlists.DoublyNode as Node
 
-    private var size: Int = 0
+class DoublyLinkedList<T> : MutableCollection<T> {
+    private var head: Node<T>? = null
+    private var tail: Node<T>? = null
 
-    fun isEmpty(): Boolean = size == 0
+    override var size: Int = 0
+        private set
+
+    override fun isEmpty(): Boolean = size == 0
 
     override fun toString(): String =
         if (isEmpty()) "Empty list"
         else head.toString()
+
+    fun push(value: T): DoublyLinkedList<T> {
+        val newNode = Node(value = value, next = head)
+        head?.prev = newNode
+        head = newNode
+        if (tail == null)
+            tail = head
+        size++
+        return this
+    }
+
+    fun append(value: T): DoublyLinkedList<T> {
+        if (isEmpty()) return push(value = value)
+        tail?.next = Node(value = value, prev = tail)
+        tail = tail?.next
+        size++
+        return this
+    }
+
+    fun nodeAt(index: Int): Node<T>? {
+        if (index < 0 || index >= size) return null
+        var currentNode = head
+        var currentIndex = 0
+        while (currentNode != null && currentIndex < index) {
+            currentNode = currentNode.next
+            currentIndex++
+        }
+        return currentNode
+    }
+
+    fun insertAfter(index: Int, value: T): DoublyLinkedList<T> {
+        val keyNode = nodeAt(index = index) ?: return this
+        if (keyNode == tail) return append(value = value)
+        val newNode = Node(value = value, prev = keyNode, next = keyNode.next)
+        keyNode.next?.prev = newNode
+        keyNode.next = newNode
+        size++
+        return this
+    }
+
+    fun pop(): T? {
+        if (!isEmpty()) size--
+        val result = head?.value
+        head = head?.next
+        head?.prev = null
+        if (isEmpty()) tail = null
+        return result
+    }
+
+    fun removeLast(): T? {
+        val result = tail?.value
+        tail = tail?.prev
+        tail?.next = null
+        size--
+        return result
+    }
+
+    fun removeAt(index: Int): T? {
+        if (index == 0) return pop()
+        val keyNode = nodeAt(index = index) ?: return null
+        val (prevNode, nextNode) = keyNode.prev to keyNode.next
+        if (tail == keyNode) tail = prevNode
+        prevNode?.next = nextNode
+        nextNode?.prev = prevNode
+        size--
+        return keyNode.value
+    }
+
+    override fun iterator(): MutableIterator<T> = LinkedListIterator(this)
+
+    class LinkedListIterator<T>(private val list: DoublyLinkedList<T>) : MutableIterator<T> {
+
+        private var lastNode: Node<T>? = null
+        private var index: Int = 0
+
+        override fun hasNext(): Boolean = index < list.size
+
+        override fun next(): T {
+            if (index >= list.size) throw ArrayIndexOutOfBoundsException()
+            lastNode = if (index == 0) list.nodeAt(index = 0) else lastNode?.next
+            index++
+            return lastNode!!.value
+        }
+
+        override fun remove() {
+            if (index == 1) list.pop()
+            else {
+                val keyNode = list.nodeAt(index = index - 1)
+                list.removeAt(index = index - 1)
+                lastNode = keyNode?.prev
+            }
+            index--
+        }
+    }
+
+    override fun contains(element: T): Boolean = this.any { it == element }
+
+    override fun containsAll(elements: Collection<T>): Boolean = elements.all { this.contains(it) }
+
+    override fun add(element: T): Boolean {
+        append(value = element)
+        return true
+    }
+
+    override fun addAll(elements: Collection<T>): Boolean = elements.all { add(element = it) }
+
+    override fun clear() {
+        head = null
+        tail = null
+        size = 0
+    }
+
+    override fun remove(element: T): Boolean {
+        val iterator = iterator()
+        while (iterator.hasNext())
+            if (iterator.next() == element) {
+                iterator.remove()
+                return true
+            }
+        return false
+    }
+
+    override fun removeAll(elements: Collection<T>): Boolean {
+        var result = false
+        for (item in elements)
+            result = remove(item) || result
+        return result
+    }
+
+    override fun retainAll(elements: Collection<T>): Boolean {
+        var result = false
+        val iterator = this.iterator()
+        while (iterator.hasNext()) {
+            val item = iterator.next()
+            if (!elements.contains(item)) {
+                iterator.remove()
+                result = true
+            }
+        }
+        return result
+    }
+
+
 }
--- src/test/kotlin/basic/linkedlists/DoublyLinkedListTest.kt
+++ src/test/kotlin/basic/linkedlists/DoublyLinkedListTest.kt
@@ -17,4 +17,201 @@ internal class DoublyLinkedListTest {
         val list = DoublyLinkedList<Int>()
         assertEquals(list.toString(), "Empty list")
     }
+
+    @Test
+    fun push() {
+        val list = DoublyLinkedList<Int>()
+        assertEquals(list.toString(), "Empty list")
+        assertTrue(list.isEmpty())
+        list.push(value = 1).push(value = 2)
+        assertFalse(list.isEmpty())
+        assertEquals(list.toString(), "2 <-> 1")
+    }
+
+    @Test
+    fun append() {
+        val list = DoublyLinkedList<Int>()
+        assertTrue(list.isEmpty())
+        assertEquals(list.toString(), "Empty list")
+        list.append(value = 1)
+        assertFalse(list.isEmpty())
+        assertEquals(list.toString(), "1")
+        list.append(value = 2).append(value = 3)
+        assertEquals(list.toString(), "1 <-> 2 <-> 3")
+    }
+
+    @Test
+    fun nodeAt() {
+        val list = DoublyLinkedList<Int>()
+        assertTrue(list.isEmpty())
+        assertNull(list.nodeAt(index = -1))
+        assertNull(list.nodeAt(index = 0))
+        list.append(value = 1)
+        assertNull(list.nodeAt(index = -1))
+        assertNull(list.nodeAt(index = 1))
+        assertNotNull(list.nodeAt(index = 0))
+        assertEquals(list.nodeAt(index = 0).toString(), "1")
+        list.append(value = 2).append(value = 3)
+        assertNotNull(list.nodeAt(0))
+        assertNotNull(list.nodeAt(1))
+        assertNotNull(list.nodeAt(2))
+        assertNull(list.nodeAt(3))
+        assertEquals(list.nodeAt(index = 0).toString(), "1 <-> 2 <-> 3")
+        assertEquals(list.nodeAt(index = 1).toString(), "2 <-> 3")
+        assertEquals(list.nodeAt(index = 2).toString(), "3")
+    }
+    
+    @Test
+    fun insertAfter() {
+        val list = DoublyLinkedList<Int>()
+        list.insertAfter(-1, 1)
+        assertEquals(list.toString(), "Empty list")
+        list.append(value = 0)
+        assertEquals(list.toString(), "0")
+        list.insertAfter(index = -1, value = 1)
+        assertEquals(list.toString(), "0")
+        list.insertAfter(index = 0, value = 1)
+        assertEquals(list.toString(), "0 <-> 1")
+        list.insertAfter(index = 0, value = 2)
+        assertEquals(list.toString(), "0 <-> 2 <-> 1")
+        list.insertAfter(index = 1, value = 3)
+        assertEquals(list.toString(), "0 <-> 2 <-> 3 <-> 1")
+        list.insertAfter(index = 1, value = 4)
+        assertEquals(list.toString(), "0 <-> 2 <-> 4 <-> 3 <-> 1")
+    }
+
+    @Test
+    fun pop() {
+        val list = DoublyLinkedList<Int>()
+        assertNull(list.pop())
+        list.append(value = 1).append(value = 2).append(value = 3)
+        assertEquals(list.toString(), "1 <-> 2 <-> 3")
+        assertFalse(list.isEmpty())
+        assertEquals(list.pop(), 1)
+        assertEquals(list.toString(), "2 <-> 3")
+        assertEquals(list.pop(), 2)
+        assertEquals(list.toString(), "3")
+        assertEquals(list.pop(), 3)
+        assertTrue(list.isEmpty())
+    }
+
+    @Test
+    fun removeLast() {
+        val list = DoublyLinkedList<Int>()
+        list.append(value = 1).append(value = 2).append(value = 3)
+        assertEquals(list.toString(), "1 <-> 2 <-> 3")
+        assertEquals(list.removeLast(), 3)
+        assertEquals(list.toString(), "1 <-> 2")
+        assertEquals(list.removeLast(), 2)
+        assertEquals(list.toString(), "1")
+        assertEquals(list.removeLast(), 1)
+        assertEquals(list.toString(), "Empty list")
+        assertTrue(list.isEmpty())
+    }
+
+    @Test
+    fun removeAt() {
+        val list = DoublyLinkedList<Int>()
+        list.append(value = 1).append(value = 2).append(value = 3).append(value = 4)
+        assertEquals(list.toString(), "1 <-> 2 <-> 3 <-> 4")
+        assertEquals(list.removeAt(index = 2), 3)
+        assertEquals(list.toString(), "1 <-> 2 <-> 4")
+        assertEquals(list.removeAt(index = 2), 4)
+        assertEquals(list.toString(), "1 <-> 2")
+        assertEquals(list.removeAt(index = 0), 1)
+        assertEquals(list.toString(), "2")
+        assertNull(list.removeAt(index = 1))
+        assertEquals(list.removeAt(index = 0), 2)
+        assertEquals(list.toString(), "Empty list")
+        assertTrue(list.isEmpty())
+    }
+
+    @Test
+    fun iterator() {
+        val list = DoublyLinkedList<Int>()
+        list.append(value = 1).append(value = 2).append(value = 3)
+        assertEquals(list.toString(), "1 <-> 2 <-> 3")
+        assertEquals(buildString {
+            for (item in list) appendLine(item)
+        }, list.toString().replace(" <-> ", "\n") + "\n")
+    }
+
+    @Test
+    fun removeIterator() {
+        val list = DoublyLinkedList<Int>()
+        list.append(value = 1).append(value = 2).append(value = 3)
+        assertEquals(list.toString(), "1 <-> 2 <-> 3")
+        val iterator = list.iterator()
+        while (iterator.hasNext())
+            iterator.remove()
+        assertFalse(iterator.hasNext())
+    }
+
+    @Test
+    fun contains() {
+        val list = DoublyLinkedList<Int>()
+        list.append(value = 1).append(value = 2).append(value = 3)
+        assertEquals(list.toString(), "1 <-> 2 <-> 3")
+        assertTrue(list.contains(1))
+        assertTrue(list.contains(2))
+        assertTrue(list.contains(3))
+        assertFalse(list.contains(4))
+    }
+
+    @Test
+    fun containsAll() {
+        val list = DoublyLinkedList<Int>()
+        list.append(value = 1).append(value = 2).append(value = 3)
+        assertEquals(list.toString(), "1 <-> 2 <-> 3")
+        val testList1 = listOf(1, 2, 3)
+        assertTrue(list.containsAll(testList1))
+        val testList2 = listOf(2, 3, 4, 5)
+        assertFalse(list.containsAll(testList2))
+    }
+
+    @Test
+    fun remove() {
+        val list = DoublyLinkedList<Int>()
+        list.append(value = 1).append(value = 2).append(value = 3)
+        assertEquals(list.toString(), "1 <-> 2 <-> 3")
+        assertTrue(list.remove(1))
+        assertEquals(list.toString(), "2 <-> 3")
+        assertFalse(list.remove(4))
+        assertEquals(list.toString(), "2 <-> 3")
+        assertTrue(list.remove(3))
+        assertEquals(list.toString(), "2")
+        assertTrue(list.remove(2))
+        assertTrue(list.isEmpty())
+        assertEquals(list.toString(), "Empty list")
+    }
+
+    @Test
+    fun removeAll() {
+        val list = DoublyLinkedList<Int>()
+        list.append(value = 1).append(value = 2).append(value = 3)
+        assertEquals(list.toString(), "1 <-> 2 <-> 3")
+        list.removeAll(listOf(2))
+        assertEquals(list.toString(), "1 <-> 3")
+        list.removeAll(listOf(2))
+        assertEquals(list.toString(), "1 <-> 3")
+        list.removeAll(listOf(3, 1))
+        assertEquals(list.toString(), "Empty list")
+        assertTrue(list.isEmpty())
+    }
+
+    @Test
+    fun retainAll() {
+        val list = DoublyLinkedList<Int>()
+        list.append(value = 1).append(value = 2).append(value = 3)
+        assertEquals(list.toString(), "1 <-> 2 <-> 3")
+        list.retainAll(listOf(3, 1))
+        assertEquals(list.toString(), "1 <-> 3")
+        list.retainAll(listOf(1))
+        assertEquals(list.toString(), "1")
+        list.retainAll(listOf(1))
+        assertEquals(list.toString(), "1")
+        list.retainAll(listOf())
+        assertEquals(list.toString(), "Empty list")
+        assertTrue(list.isEmpty())
+    }
 }
