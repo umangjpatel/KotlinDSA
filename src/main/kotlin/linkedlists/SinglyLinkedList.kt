03d1e67055d41aa8f3e45cd0f89fd288112e74f5
umangjpatel <umangpatel1947@gmail.com>
2022-01-06 09:52:19 -0500

    SinglyLinkedList insertion operations

--- src/main/kotlin/linkedlists/SinglyLinkedList.kt
+++ src/main/kotlin/linkedlists/SinglyLinkedList.kt
@@ -12,4 +12,42 @@ class SinglyLinkedList<T> {
 
     override fun toString(): String =
         if (isEmpty()) "Empty list" else head.toString()
+
+    fun push(value: T): SinglyLinkedList<T> {
+        head = Node(value = value, next = head)
+        if (tail == null)
+            tail = head
+        size++
+        return this
+    }
+
+    fun append(value: T): SinglyLinkedList<T> {
+        if (isEmpty())
+            return push(value = value)
+        tail?.next = Node(value = value, next = null)
+        tail = tail?.next
+        size++
+        return this
+    }
+
+    fun nodeAt(index: Int): Node<T>? {
+        if (index < 0 || index >= this.size)
+            return null
+        var current = head
+        var currentIndex = 0
+        while (current != null && currentIndex < index) {
+            current = current.next
+            currentIndex++
+        }
+        return current
+    }
+
+    fun insertAfter(index: Int, value: T): SinglyLinkedList<T> {
+        val afterNode = nodeAt(index = index) ?: return this
+        afterNode.next = Node(value = value, next = afterNode.next)
+        if (afterNode == tail)
+            tail = afterNode.next
+        size++
+        return this
+    }
 }
--- src/test/kotlin/linkedlists/SinglyLinkedListTest.kt
+++ src/test/kotlin/linkedlists/SinglyLinkedListTest.kt
@@ -1,18 +1,94 @@
 package linkedlists
 
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
 
 internal class SinglyLinkedListTest {
 
-    @org.junit.jupiter.api.Test
+    @Test
     fun testToString() {
         val list = SinglyLinkedList<Int>()
         assertEquals(list.toString(), "Empty list")
+        list.push(1)
+        assertEquals(list.toString(), "1")
+        list.append(2)
+        assertEquals(list.toString(), "1 -> 2")
+        list.insertAfter(0, 3)
+        assertEquals(list.toString(), "1 -> 3 -> 2")
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     fun testIsEmpty() {
         val list = SinglyLinkedList<Int>()
         assertTrue(list.isEmpty())
+        list.push(1)
+        assertFalse(list.isEmpty())
+        list.append(2)
+        assertFalse(list.isEmpty())
+        list.insertAfter(0, 3)
+        assertFalse(list.isEmpty())
+    }
+
+    @Test
+    fun testSize() {
+        val list = SinglyLinkedList<Int>()
+        assertEquals(list.size, 0)
+        list.push(1)
+        assertEquals(list.size, 1)
+        list.append(2)
+        assertEquals(list.size, 2)
+        list.insertAfter(0, 3)
+        assertEquals(list.size, 3)
+    }
+
+    @Test
+    fun testPush() {
+        val list = SinglyLinkedList<Int>()
+        assertTrue(list.isEmpty())
+        list.push(1)
+        list.push(2)
+        list.push(3)
+        assertFalse(list.isEmpty())
+        assertEquals(list.toString(), "3 -> 2 -> 1")
+    }
+
+    @Test
+    fun testAppend() {
+        val list = SinglyLinkedList<Int>()
+        assertTrue(list.isEmpty())
+        list.append(1)
+        list.append(2)
+        list.append(3)
+        assertEquals(list.toString(), "1 -> 2 -> 3")
+    }
+
+    @Test
+    fun testNodeAt() {
+        val list = SinglyLinkedList<Int>()
+        assertTrue(list.isEmpty())
+        assertNull(list.nodeAt(0))
+        assertNull(list.nodeAt(1))
+        list.append(1).append(2).append(3)
+        assertEquals(list.nodeAt(1).toString(), "2 -> 3")
+        assertEquals(list.nodeAt(0).toString(), "1 -> 2 -> 3")
+        assertEquals(list.nodeAt(2).toString(), "3")
+        assertNull(list.nodeAt(-1))
+        assertNull(list.nodeAt(3))
+    }
+
+    @Test
+    fun testInsertAfter() {
+        val list = SinglyLinkedList<Int>().apply {
+            append(1)
+            append(2)
+            append(3)
+        }
+        assertEquals(list.toString(), "1 -> 2 -> 3")
+        list.insertAfter(0, 4)
+        assertEquals(list.toString(), "1 -> 4 -> 2 -> 3")
+        list.insertAfter(3, 5)
+        assertEquals(list.toString(), "1 -> 4 -> 2 -> 3 -> 5")
+        list.append(6)
+        assertEquals(list.toString(), "1 -> 4 -> 2 -> 3 -> 5 -> 6")
     }
 }
