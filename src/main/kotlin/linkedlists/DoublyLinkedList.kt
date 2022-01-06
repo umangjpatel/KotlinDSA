f0b210f2eb33d93941ea8003e06240f283166b1c
umangjpatel <umangpatel1947@gmail.com>
2022-01-06 15:34:11 -0500

    DoublyLinkedList insertion operations

--- src/main/kotlin/linkedlists/DoublyLinkedList.kt
+++ src/main/kotlin/linkedlists/DoublyLinkedList.kt
@@ -1,3 +1,5 @@
+@file:Suppress("DuplicatedCode")
+
 package linkedlists
 
 import linkedlists.DoublyNode as Node
@@ -13,4 +15,45 @@ class DoublyLinkedList<T> {
     override fun toString(): String =
         if (isEmpty()) "Empty list"
         else head.toString()
+
+    fun push(value: T): DoublyLinkedList<T> {
+        head = Node(value = value, next = head)
+        head?.next?.prev = head
+        if (isEmpty())
+            tail = head
+        size++
+        return this
+    }
+
+    fun append(value: T): DoublyLinkedList<T> {
+        if (isEmpty())
+            return push(value = value)
+        tail?.next = Node(value = value, prev = tail)
+        tail = tail?.next
+        size++
+        return this
+    }
+
+    fun nodeAt(index: Int): Node<T>? {
+        if (index < 0 || index >= this.size)
+            return null
+        var current = head
+        var currentIndex = 0
+        while (current != null && currentIndex < index) {
+            current = current.next
+            currentIndex++
+        }
+        return current
+    }
+
+    fun insertAfter(index: Int, value: T): DoublyLinkedList<T> {
+        val afterNode = nodeAt(index = index) ?: return this
+        val node = Node(value = value, prev = afterNode, next = afterNode.next)
+        if (tail == afterNode)
+            tail = node
+        afterNode.next?.prev = node
+        afterNode.next = node
+        size++
+        return this
+    }
 }
--- src/test/kotlin/linkedlists/DoublyLinkedListTest.kt
+++ src/test/kotlin/linkedlists/DoublyLinkedListTest.kt
@@ -8,5 +8,90 @@ internal class DoublyLinkedListTest {
 
     @Test
     fun testToString() {
+        val list = DoublyLinkedList<Int>()
+        assertEquals(list.toString(), "Empty list")
+        list.push(1)
+        assertEquals(list.toString(), "1")
+        list.append(2)
+        assertEquals(list.toString(), "1 <-> 2")
+        list.insertAfter(0, 3)
+        assertEquals(list.toString(), "1 <-> 3 <-> 2")
     }
+
+    @Test
+    fun testIsEmpty() {
+        val list = DoublyLinkedList<Int>()
+        assertTrue(list.isEmpty())
+        list.push(1)
+        assertFalse(list.isEmpty())
+        list.append(2)
+        assertFalse(list.isEmpty())
+        list.insertAfter(0, 3)
+        assertFalse(list.isEmpty())
+    }
+
+    @Test
+    fun testSize() {
+        val list = DoublyLinkedList<Int>()
+        assertEquals(list.size, 0)
+        list.push(1)
+        assertEquals(list.size, 1)
+        list.append(2)
+        assertEquals(list.size, 2)
+        list.insertAfter(0, 3)
+        assertEquals(list.size, 3)
+    }
+
+    @Test
+    fun testPush() {
+        val list = DoublyLinkedList<Int>()
+        assertTrue(list.isEmpty())
+        list.push(1)
+        list.push(2)
+        list.push(3)
+        assertFalse(list.isEmpty())
+        assertEquals(list.toString(), "3 <-> 2 <-> 1")
+    }
+
+    @Test
+    fun testAppend() {
+        val list = DoublyLinkedList<Int>()
+        assertTrue(list.isEmpty())
+        list.append(1)
+        list.append(2)
+        list.append(3)
+        assertFalse(list.isEmpty())
+        assertEquals(list.toString(), "1 <-> 2 <-> 3")
+    }
+
+    @Test
+    fun testNodeAt() {
+        val list = DoublyLinkedList<Int>()
+        assertTrue(list.isEmpty())
+        assertNull(list.nodeAt(0))
+        assertNull(list.nodeAt(1))
+        list.append(1).append(2).append(3)
+        assertEquals(list.nodeAt(1).toString(), "2 <-> 3")
+        assertEquals(list.nodeAt(0).toString(), "1 <-> 2 <-> 3")
+        assertEquals(list.nodeAt(2).toString(), "3")
+        assertNull(list.nodeAt(-1))
+        assertNull(list.nodeAt(3))
+    }
+
+    @Test
+    fun testInsertAfter() {
+        val list = DoublyLinkedList<Int>().apply {
+            append(1)
+            append(2)
+            append(3)
+        }
+        assertEquals(list.toString(), "1 <-> 2 <-> 3")
+        list.insertAfter(0, 4)
+        assertEquals(list.toString(), "1 <-> 4 <-> 2 <-> 3")
+        list.insertAfter(3, 5)
+        assertEquals(list.toString(), "1 <-> 4 <-> 2 <-> 3 <-> 5")
+        list.append(6)
+        assertEquals(list.toString(), "1 <-> 4 <-> 2 <-> 3 <-> 5 <-> 6")
+    }
+
 }
