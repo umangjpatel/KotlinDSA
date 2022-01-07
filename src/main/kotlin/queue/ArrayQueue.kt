e895ee476bae54d2b82f0566f9bc4491b7b62ee1
umangjpatel <umangpatel1947@gmail.com>
2022-01-07 12:12:54 -0500

    ArrayQueue implemented and tested

---
+++ src/main/kotlin/queue/ArrayQueue.kt
@@ -0,0 +1,21 @@
+package queue
+
+class ArrayQueue<T>: Queue<T> {
+
+    private val storage = mutableListOf<T>()
+
+    override val count: Int
+        get() = storage.size
+
+    override fun enqueue(element: T) {
+        storage.add(element = element)
+    }
+
+    override fun dequeue(): T? = storage.removeFirstOrNull()
+
+    override fun first(): T? = storage.firstOrNull()
+
+    override fun toString(): String =
+        if (isEmpty()) "Empty queue"
+        else storage.joinToString(separator = ", ")
+}
